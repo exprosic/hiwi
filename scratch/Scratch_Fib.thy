4dc77825b4f0ed0e0fdfd55012141aa2dfaa440c
exprosic <exprosic@gmail.com>
2017-11-12 23:45:13 +0100

    fib refined

--- scratch/Scratch_Fib.thy
+++ scratch/Scratch_Fib.thy
@@ -16,6 +16,16 @@ term trimono
 
 definition fib_rec_body :: "(nat \<Rightarrow> int nres) \<Rightarrow> nat \<Rightarrow> int nres" where
   "fib_rec_body \<equiv> \<lambda>f n.
+    if n < 2
+      then RETURN 1
+      else do {
+        f0 \<leftarrow> f (n-2);
+        f1 \<leftarrow> f (n-1);
+        RETURN (f0+f1)
+      }"
+
+definition fib_rec_body' :: "(nat \<Rightarrow> int nres) \<Rightarrow> nat \<Rightarrow> int nres" where
+  "fib_rec_body \<equiv> \<lambda>f n.
     case n of
       0 \<Rightarrow> RETURN 1
     | Suc 0 \<Rightarrow> RETURN 1
@@ -24,7 +34,6 @@ definition fib_rec_body :: "(nat \<Rightarrow> int nres) \<Rightarrow> nat \<Rig
         f1 \<leftarrow> f (Suc n');
         RETURN (f0+f1)
       }"
-
 definition fib_rec :: "nat \<Rightarrow> int nres" where
   "fib_rec n \<equiv> RECT fib_rec_body n"
 
@@ -38,9 +47,10 @@ lemma fib_rec_refine: "(fib_rec, fib_spec) \<in> Id \<rightarrow> \<langle>Id\<r
   subgoal premises prems for _ f n
     thm prems(1)[simplified] prems(3)[symmetric]
     unfolding fib_rec_body_def
-    apply (clarsimp split: nat.split)
-    unfolding RETURN_def[symmetric]
-    apply (refine_vcg)
+    apply (refine_vcg order_trans[OF prems(1)]; cases n rule: fib.cases; simp)
+done
+
+    term 0 (*
     subgoal premises prems'[simp] for m
       thm prems(1)[of m, simplified] prems(1)[of "Suc m", simplified]
       apply (rule order_trans)
@@ -54,5 +64,25 @@ lemma fib_rec_refine: "(fib_rec, fib_spec) \<in> Id \<rightarrow> \<langle>Id\<r
       done
     done
   done
+term 0 (*
+sepref_definition fib_rec1 is fib_rec :: "nat_assn\<^sup>d \<rightarrow>\<^sub>a int_assn"
+  unfolding fib_rec_def fib_rec_body_def
+  apply sepref_dbg_keep
+      apply sepref_dbg_trans_keep
+        apply sepref_dbg_trans_step_keep
 
-end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
