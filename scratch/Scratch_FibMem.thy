2f56ac23cade13403277ae7c5baff05ffb968f63
exprosic <exprosic@gmail.com>
2017-11-14 13:57:36 +0100

    fibmem almost finished

--- scratch/Scratch_FibMem.thy
+++ scratch/Scratch_FibMem.thy
@@ -7,70 +7,215 @@ fun fib :: "nat \<Rightarrow> int" where
 | "fib (Suc 0) = 1"
 | "fib (Suc (Suc n)) = fib (Suc n) + fib n"
 
+lemma [simp]:
+  "n < 2 \<Longrightarrow> fib n = 1"
+  "\<not>(n < 2) \<Longrightarrow> fib n = fib (n-2) + fib (n-1)"
+  by (cases n rule: fib.cases; simp)+
+
+definition fib_spec :: "nat \<Rightarrow> int nres" where
+  "fib_spec n \<equiv> RETURN (fib n)"
+
 type_synonym tab = "int list"
 
 (*Since fib n \<ge> 0, negative value is used here to represent None*)
 definition is_some :: "int \<Rightarrow> bool" where
   "is_some v \<equiv> v \<ge> 0"
 
-definition checkmem :: "tab \<Rightarrow> nat \<Rightarrow> (tab \<times> int) nres \<Rightarrow> (tab \<times> int) nres" where
+definition checkmem :: "tab \<Rightarrow> nat \<Rightarrow> (int \<times> tab) nres \<Rightarrow> (int \<times> tab) nres" where
   "checkmem M n v \<equiv> do {
     fn \<leftarrow> mop_list_get M n;
     if is_some fn
-      then RETURN (M, fn)
+      then RETURN (fn, M)
       else do {
-        (M, fn) \<leftarrow> v;
+        (fn, M) \<leftarrow> v;
         M \<leftarrow> mop_list_set M n fn;
-        RETURN (M, fn)
+        RETURN (fn, M)
       }
     }"
 
-definition fib_rec_mem_body :: "((tab \<times> nat) \<Rightarrow> (tab \<times> int) nres) \<Rightarrow> (tab \<times> nat) \<Rightarrow> (tab \<times> int) nres" where
-  "fib_rec_mem_body \<equiv> \<lambda>f (M, n). checkmem M n (do {
+definition fib_rec_mem_body :: "((nat \<times> tab) \<Rightarrow> (int \<times> tab) nres) \<Rightarrow> ((nat \<times> tab) \<Rightarrow> (int \<times> tab) nres)" where
+  "fib_rec_mem_body \<equiv> \<lambda>f (n, M). checkmem M n (do {
     if n < 2
-      then RETURN (M, 1)
+      then RETURN (1, M)
       else do {
-        (M, f0) \<leftarrow> f (M, n-2);
-        (M, f1) \<leftarrow> f (M, n-1);
-        RETURN (M, f0 + f1)
+        (f0, M) \<leftarrow> f (n-2, M);
+        (f1, M) \<leftarrow> f (n-1, M);
+        RETURN (f0 + f1, M)
       }
-    })"
+  })"
 
 definition fib_rec_mem :: "nat \<Rightarrow> int nres" where
   "fib_rec_mem n \<equiv> do {
-    (_, v) \<leftarrow> RECT fib_rec_mem_body (replicate (n+1) (-1), n);
+    (v, _) \<leftarrow> RECT fib_rec_mem_body (n, replicate (n+1) (-1));
     RETURN v
-   }"
+  }"
 
 sepref_definition fib_rec_mem1 is fib_rec_mem :: "nat_assn\<^sup>k \<rightarrow>\<^sub>a int_assn"
   unfolding fib_rec_mem_def fib_rec_mem_body_def checkmem_def is_some_def
-  apply (rewrite in "RECT _ (\<hole>, _)" array_fold_custom_of_list)
+  apply (rewrite in "RECT _ (_, \<hole>_)" array_fold_custom_of_list)
   by sepref
 
-definition fib_rec_mem_body' where
-  "fib_rec_mem_body' \<equiv> \<lambda>f n. "
+(*
+abbreviation "nres_bind \<equiv> Refine_Basic.bind"
+abbreviation "state_bind \<equiv> Monad.bind"
+*)
 
-term 0 (*
+datatype ('M, 'a) stateT = StateT (runStateT: "'M \<Rightarrow> ('a \<times> 'M) nres")
+term 0 (**)
+
+definition returnT :: "'a \<Rightarrow> ('M, 'a) stateT" where
+  "returnT a \<equiv> StateT (\<lambda>M. RETURN (a, M))"
+
+definition bindT :: "('M, 'a) stateT \<Rightarrow> ('a \<Rightarrow> ('M, 'b) stateT) \<Rightarrow> ('M, 'b) stateT" where
+  "bindT s f \<equiv> StateT (\<lambda>M. do {
+    (v, M') \<leftarrow> runStateT s M;
+    runStateT (f v) M'
+  })"
+
+adhoc_overloading
+  Monad_Syntax.bind bindT
+
+definition getT :: "('M, 'M) stateT" where
+  "getT \<equiv> StateT (\<lambda>M. RETURN (M, M))"
+
+definition putT :: "'M \<Rightarrow> ('M, unit) stateT" where
+  "putT M \<equiv> StateT (\<lambda>M. RETURN ((), M))"
+
+definition liftT :: "'a nres \<Rightarrow> ('M, 'a) stateT" where
+  "liftT nr = StateT (\<lambda>M. do {v \<leftarrow> nr; RETURN (v, M)})"
+
+definition checkmemT :: "nat \<Rightarrow> (tab, int) stateT \<Rightarrow> (tab, int) stateT" where
+  "checkmemT n s \<equiv> do {
+    M \<leftarrow> getT;
+    Mn \<leftarrow> liftT (mop_list_get M n);
+    if is_some Mn
+      then returnT Mn
+      else do {
+        v \<leftarrow> s;
+        M' \<leftarrow> liftT (mop_list_set M n v);
+        putT M';
+        returnT v
+      }
+  }"
+
+definition fib_rec_mem_body' :: "((nat \<times> tab) \<Rightarrow> (int \<times> tab) nres) \<Rightarrow> ((nat \<times> tab) \<Rightarrow> (int \<times> tab) nres)" where
+  "fib_rec_mem_body' \<equiv> \<lambda>f (n, M). runStateT (checkmemT n (do {
+    if n<2
+      then returnT 1
+      else do {
+        f0 \<leftarrow> StateT (curry f (n-2));
+        f1 \<leftarrow> StateT (curry f (n-1));
+        returnT (f0 + f1)
+      }
+    })) M"
+
+definition fib_rec_mem' :: "nat \<Rightarrow> int nres" where
+  "fib_rec_mem' n \<equiv> do {
+    (v, _) \<leftarrow> RECT fib_rec_mem_body' (n, replicate (n+1) (-1));
+    RETURN v
+  }"
 
 definition cmem :: "tab \<Rightarrow> bool" where
-  "cmem M \<equiv> \<forall>i. i<length M \<longrightarrow> M!i\<ge>0 \<longrightarrow> M!i=fib i"
+  "cmem M \<equiv> \<forall>i. i<length M \<longrightarrow> is_some (M!i) \<longrightarrow> M!i=fib i"
 
 lemma cmem_intro:
-  assumes "\<And>i. i<length M \<Longrightarrow> M!i\<ge>0 \<Longrightarrow> M!i=fib i"
+  assumes "\<And>i. i<length M \<Longrightarrow> is_some (M!i) \<Longrightarrow> M!i=fib i"
   shows "cmem M"
-  using assms unfolding cmem_def by fastforce
+  using assms unfolding cmem_def is_some_def by fastforce
 
 lemma cmem_elim:
-  assumes "cmem M" "i < length M"
-  obtains "M!i < 0" | "fib i = M!i "
+  assumes "cmem M" "i < length M" "is_some (M!i)"
+  obtains "fib i = M!i "
   using assms unfolding cmem_def by fastforce
-  term 0 
 
 lemma cmem_update:
   "\<lbrakk>cmem M; v = fib i\<rbrakk> \<Longrightarrow> cmem (list_update M i v)"
   by (fastforce intro!: cmem_intro elim: cmem_elim simp: nth_list_update')
 
-definition crel_vs :: "'a nres \<Rightarrow> (tab \<times> 'a) nres \<Rightarrow> bool" where
-  "crel_vs v s \<equiv> nofail v \<longrightarrow> the"
+definition crel_vs :: "'a nres \<Rightarrow> (tab, 'a) stateT \<Rightarrow> bool" where
+  "crel_vs nr s \<equiv> \<forall>M. cmem M \<longrightarrow> (case runStateT s M of
+    FAILi \<Rightarrow> nr=FAIL
+  | RES vM \<Rightarrow> RES (fst`vM) \<le> nr \<and> Ball (snd`vM) cmem)"
 
-end
+context
+  fixes N :: nat
+begin
+
+definition param_isvalid :: "nat \<Rightarrow> tab \<Rightarrow> bool" where
+  "param_isvalid n M \<equiv> n < length M \<and> length M = N \<and> cmem M"
+
+definition fib_mem_spec :: "(nat \<times> tab) \<Rightarrow> (int \<times> tab) nres" where
+  "fib_mem_spec \<equiv> \<lambda>(n, M). SPEC (\<lambda>(v, M'). fib n = v \<and> param_isvalid n M')"
+
+lemma checkmemT_vcg_rule:
+  fixes n s M spec
+  assumes "param_isvalid n M \<Longrightarrow> runStateT s M \<le> fib_mem_spec (n, M)" "param_isvalid n M"
+  shows "runStateT (checkmemT n s) M \<le> fib_mem_spec (n, M)"
+  unfolding checkmemT_def bindT_def getT_def putT_def liftT_def
+  apply auto
+  subgoal
+    apply (refine_vcg)
+    apply (unfold returnT_def RETURN_def fib_mem_spec_def)
+    using assms(2) apply (auto simp: param_isvalid_def elim: cmem_elim)
+    done
+  subgoal
+    apply (refine_vcg)
+    subgoal using assms(2) by (auto simp: param_isvalid_def)
+    apply (unfold returnT_def RETURN_def fib_mem_spec_def)
+    apply refine_vcg
+    apply clarsimp
+    apply (intro assms(1)[unfolded fib_mem_spec_def, simplified] assms(2))
+    done
+  done
+
+lemma blah: "runStateT (if b then StateT sx else StateT sy) = (if b then sx else sy)"
+  by auto
+
+lemma "(RECT fib_rec_mem_body', fib_mem_spec) \<in> (br id (uncurry param_isvalid)) \<rightarrow> \<langle>Id\<times>\<^sub>rId\<rangle>nres_rel"
+  apply (clarsimp simp: uncurry_def in_br_conv intro!: nres_relI)
+  apply (rule RECT_rule[where V="fst <*mlex*> {}" and pre="uncurry param_isvalid"])
+  defer
+  subgoal by (simp add: wf_mlex)
+  subgoal by (simp add: param_isvalid_def)
+  subgoal premises prems for n M f nM
+    thm prems
+    apply (unfold fib_rec_mem_body'_def)
+    apply (auto split: prod.split)
+    subgoal for x' M'
+      apply (rule checkmemT_vcg_rule)
+      defer
+      subgoal using prems(3) unfolding param_isvalid_def by simp
+      subgoal unfolding fib_mem_spec_def returnT_def
+        apply refine_vcg
+        apply auto
+        done
+      done
+    subgoal for n' M'
+      apply (rule checkmemT_vcg_rule)
+      defer
+      subgoal using prems(3) unfolding param_isvalid_def by auto
+      subgoal unfolding fib_mem_spec_def returnT_def bindT_def
+        apply (refine_vcg )
+        apply (unfold stateT.sel)
+        apply (unfold curry_def)
+        apply (refine_vcg order_trans[OF prems(2)])
+        subgoal using prems(3) unfolding param_isvalid_def by auto
+        subgoal using prems(3) unfolding param_isvalid_def by (auto simp: mlex_eq)
+        subgoal unfolding fib_mem_spec_def param_isvalid_def
+          apply (refine_vcg order_trans[OF prems(2)])
+          subgoal unfolding param_isvalid_def by auto
+          subgoal by (auto simp: mlex_eq)
+          subgoal unfolding fib_mem_spec_def param_isvalid_def apply auto done
+          done
+        done
+      done
+    done
+  subgoal
+    unfolding fib_rec_mem_body'_def checkmemT_def getT_def putT_def bindT_def liftT_def curry_def returnT_def
+    is_some_def
+    unfolding blah
+    unfolding stateT.sel
+    apply auto
+    apply refine_mono
+      oops
+
--- scratch/dp/Monad.thy
+++ scratch/dp/Monad.thy
@@ -1,5 +1,5 @@
 theory Monad
-  imports Main
+  imports Main "HOL-Library.Monad_Syntax" 
 begin
 
 datatype ('M, 'a) state = State (runState: "'M \<Rightarrow> 'a \<times> 'M")
@@ -9,13 +9,12 @@ definition return :: "'a \<Rightarrow> ('M, 'a) state" where
   "return a \<equiv> State (\<lambda>M. (a, M))"
 term 0 (**)
 
-definition bind :: "('M, 'a) state \<Rightarrow> ('a \<Rightarrow> ('M, 'b) state) \<Rightarrow> ('M, 'b) state" (infixl "\<bind>" 54) where
-  "s \<bind> k \<equiv> State (\<lambda>M. let (a, M') = runState s M in runState (k a) M')"
+definition bind :: "('M, 'a) state \<Rightarrow> ('a \<Rightarrow> ('M, 'b) state) \<Rightarrow> ('M, 'b) state" where
+  "bind s k \<equiv> State (\<lambda>M. let (a, M') = runState s M in runState (k a) M')"
 term 0 (**)
   
-abbreviation then_monad :: "('M, 'a) state \<Rightarrow> ('M, 'b) state \<Rightarrow> ('M, 'b) state" (infixl "\<then>" 54) where
-  "s \<then> s' \<equiv> s \<bind> (\<lambda>_. s')"
-term 0 (**)
+adhoc_overloading
+  Monad_Syntax.bind bind
 
 definition get :: "('M, 'M) state" where
   "get \<equiv> State (\<lambda>M. (M, M))"
@@ -24,31 +23,9 @@ definition put :: "'M \<Rightarrow> ('M, unit) state" where
   "put M \<equiv> State (\<lambda>_. ((), M))"
 term 0 (**)
   
-nonterminal exec_binds and exec_bind
-syntax
-  "_exec_block" :: "exec_binds \<Rightarrow> 'a" ("exec {//(2  _)//}" [12] 62)
-  "_exec_bind"  :: "[pttrn, 'a] \<Rightarrow> exec_bind" ("(2_ \<leftarrow>/ _)" 13)
-  "_exec_let" :: "[pttrn, 'a] \<Rightarrow> exec_bind" ("(2let _ =/ _)" [1000, 13] 13)
-  "_exec_then" :: "'a \<Rightarrow> exec_bind" ("_" [14] 13)
-  "_exec_final" :: "'a \<Rightarrow> exec_binds" ("_")
-  "_exec_cons" :: "[exec_bind, exec_binds] \<Rightarrow> exec_binds" ("_;//_" [13, 12] 12)
-
-translations
-  "_exec_block (_exec_cons (_exec_then t) (_exec_final e))"
-    \<rightleftharpoons> "CONST then_monad t e"
-  "_exec_block (_exec_cons (_exec_bind p t) (_exec_final e))"
-    \<rightleftharpoons> "CONST bind t (\<lambda>p. e)"
-  "_exec_block (_exec_cons (_exec_let p t) bs)"
-    \<rightleftharpoons> "let p = t in _exec_block bs"
-  "_exec_block (_exec_cons b (_exec_cons c cs))"
-    \<rightleftharpoons> "_exec_block (_exec_cons b (_exec_final (_exec_block (_exec_cons c cs))))"
-  "_exec_cons (_exec_let p t) (_exec_final s)"
-    \<rightleftharpoons> "_exec_final (let p = t in s)"
-  "_exec_block (_exec_final e)" \<rightharpoonup> "e"
-
-term 0 (**)
 lemma bind_assoc:
-  "(s \<bind> k0) \<bind> k1 = s \<bind> (\<lambda>a. k0 a \<bind> k1)"
+  fixes s::"('M, 'a) state" and k0 k1
+  shows "(s \<bind> k0) \<bind> k1 = s \<bind> (\<lambda>a. k0 a \<bind> k1)"
   unfolding bind_def by (auto split: prod.split)
 
 lemma left_identity:
